ee8e0a2a87b79fec1a2981da8fd4247da74f3a8d
Evg <budo@narod.ru>
2018-02-28 10:42:16 +0300

    Add files via upload

---
+++ plugin.rb
@@ -0,0 +1,47 @@
+# name: private-categories
+# about: Show private categories in category list and allow requesting access
+# version: 0.0.1
+# authors: Evg
+
+enabled_site_setting :show_private_categories
+
+after_initialize do
+
+  Category.class_eval do
+    attr_accessor :is_private
+  end
+
+  CategoryDetailedSerializer.class_eval do
+    attributes :is_private
+
+    def is_private
+      object.is_private
+    end
+  end
+
+  module CategoryListExtensions
+    def initialize(guardian=nil, options = {})
+      if SiteSetting.show_private_categories
+        $old_scope = Category.method(:secured)
+        Category.define_singleton_method(:secured) { |guardian| all }
+      end
+
+      super(guardian, options)
+
+      if SiteSetting.show_private_categories
+        Category.define_singleton_method($old_scope.name, &$old_scope)
+      end
+    end
+
+    def find_categories
+      super
+      @categories.each do |category|
+        category.is_private = !@guardian.can_see_category?(category)
+      end
+    end
+  end
+
+  CategoryList.class_eval do
+    prepend CategoryListExtensions
+  end
+end
