8c0ffcfbde87787bb9433eae2ea1793824fec31b
Evg <budo@narod.ru>
2018-05-05 11:37:16 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -9,6 +9,8 @@ after_initialize do
 
   # add info category
   add_to_serializer(:basic_category, :categorization, false) { object.custom_fields['categorization'] }
+  add_to_serializer(:basic_category, :categorqa, false) { object.custom_fields['categorqa'] }
+  
   
   Category.class_eval do
     attr_accessor :is_private
