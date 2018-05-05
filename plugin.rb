b03d6a05bbdba69a8d2c75d64ef2bea572ce17df
Evg <budo@narod.ru>
2018-05-05 17:46:51 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -11,43 +11,4 @@ after_initialize do
   add_to_serializer(:basic_category, :categorization, false) { object.custom_fields['categorization'] }
   add_to_serializer(:basic_category, :categorqa, false) { object.custom_fields['categorqa'] }
   
-  
-  Category.class_eval do
-    attr_accessor :is_private
-  end
-
-    
-  CategoryDetailedSerializer.class_eval do
-    attributes :is_private
-
-    def is_private
-      object.is_private
-    end
-  end
-
-  module CategoryListExtensions
-    def initialize(guardian=nil, options = {})
-      if SiteSetting.show_private_categories
-        $old_scope = Category.method(:secured)
-        Category.define_singleton_method(:secured) { |guardian| all }
-      end
-
-      super(guardian, options)
-
-      if SiteSetting.show_private_categories
-        Category.define_singleton_method($old_scope.name, &$old_scope)
-      end
-    end
-
-    def find_categories
-      super
-      @categories.each do |category|
-        category.is_private = !@guardian.can_see_category?(category)
-      end
-    end
-  end
-
-  CategoryList.class_eval do
-    prepend CategoryListExtensions
-  end
 end
