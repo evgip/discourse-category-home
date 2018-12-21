8434f8ac4821e68fd2948f34c1f382631ce232c3
Evg <budo@narod.ru>
2018-12-21 18:15:30 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,18 +1,14 @@
-# name: category
-# about: Category list
+# name: discourse-category-home
+# about: The home category
 # version: 0.0.1
 # authors: Evg
 
 register_asset "stylesheets/portal.scss"
 
-enabled_site_setting :show_private_categories
-
 after_initialize do
 
   # add info category
-  add_to_serializer(:basic_category, :categorization, false) { object.custom_fields['categorization'] }
-  add_to_serializer(:basic_category, :categorqa, false) { object.custom_fields['categorqa'] }
   add_to_serializer(:basic_category, :categorimg, false) { object.custom_fields['categorimg'] }
   add_to_serializer(:basic_category, :categordiscr, false) { object.custom_fields['categordiscr'] }
-  add_to_serializer(:basic_category, :categortxt, false) { object.custom_fields['categortxt'] }  
+
 end
