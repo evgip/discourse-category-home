8a260a29f65ff2d4658729c9813b96bc041d11a4
Evg <budo@narod.ru>
2018-12-21 19:56:30 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -7,6 +7,11 @@ register_asset "stylesheets/portal.scss"
 
 after_initialize do
 
+  add_to_class :post, :excerpt_for_topic do
+      Post.excerpt(cooked, 300, strip_links: true)
+  end
+  add_to_serializer(:listable_topic, :include_excerpt?) { true }
+  
   # add info category
   add_to_serializer(:basic_category, :categorimg, false) { object.custom_fields['categorimg'] }
   add_to_serializer(:basic_category, :categordiscr, false) { object.custom_fields['categordiscr'] }
